9b6ebb80e5c55348a9a8aae93bf84198526039cb
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-03-24 20:19:07 +0900

    Add and reloacate files

---
+++ src/main/java/com/skuralll/depositvault/DepositVault.java
@@ -0,0 +1,89 @@
+package com.skuralll.depositvault;
+
+import com.skuralll.depositvault.db.Database;
+import java.util.logging.Logger;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DepositVault extends JavaPlugin {
+
+  private static DepositVault instance = null;
+  // Database Handler
+  private static Database db;
+
+  private static final Logger log = Logger.getLogger("Minecraft");
+
+  // Vault Economy API
+  private static Economy economy;
+  // Vault Permission API
+  private static Permission permission;
+
+  @Override
+  public void onEnable() {
+    instance = this;
+
+    // set up vault
+    if (!setupEconomy()) {
+      log.severe(String.format("[%s] - Disabled due to no Vault dependency found!",
+          getDescription().getName()));
+      getServer().getPluginManager().disablePlugin(this);
+      return;
+    }
+    setupPermissions();
+
+    // set up database
+    if (!setupDatabase()) {
+      log.severe(String.format("[%s] - Disabled due to no database connection!",
+          getDescription().getName()));
+      getServer().getPluginManager().disablePlugin(this);
+      return;
+    }
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+
+  // set up vault economy api
+  private boolean setupEconomy() {
+    if (getServer().getPluginManager().getPlugin("Vault") == null) {
+      return false;
+    }
+    RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager()
+        .getRegistration(Economy.class);
+    if (rsp == null) {
+      return false;
+    }
+    economy = rsp.getProvider();
+    return economy != null;
+  }
+
+  // set up vault permission api
+  private boolean setupPermissions() {
+    RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager()
+        .getRegistration(Permission.class);
+    permission = rsp.getProvider();
+    return permission != null;
+  }
+
+  // set up database
+  private boolean setupDatabase() {
+    Database _db = new Database("mariaDB", 3306, "root", "root", "deposit_vault");
+    if (_db.openConnection()) {
+      db = _db;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /*Getter and Setter*/
+
+  public static DepositVault getInstance() {
+    return instance;
+  }
+
+}
