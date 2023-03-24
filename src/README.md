555a0e363b58e092a37bbc019c3856909d893b44
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-03-24 20:08:45 +0900

    MySQL Connection

--- src/README.md
+++ src/README.md
@@ -1,6 +1,11 @@
 # DepositVault
+
 ## About
+
 Deposits can be made to temporarily lock the chest.
+
 ## Dependency
+
 - [Vault](https://github.com/MilkBowl/Vault) ＆ [VaultAPI](https://github.com/MilkBowl/VaultAPI).
-- Economic plugin that can work with Vault. (I recommend [Jeacon](https://github.com/HimaJyun/Jecon))
+- Economic plugin that can work with Vault. (I
+  recommend [Jeacon](https://github.com/HimaJyun/Jecon))
---
+++ src/main/java/com/skuralll/depositvault/db/Database.java
@@ -0,0 +1,44 @@
+package com.skuralll.depositvault.db;
+
+import com.skuralll.depositvault.DepositVault;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+  // Database connection information
+  private String host;
+  private int port;
+  private String user;
+  private String password;
+  private String database;
+
+  // connection
+  private Connection connection;
+
+  public Database(String host, int port, String user, String password, String database) {
+    this.host = host;
+    this.port = port;
+    this.user = user;
+    this.password = password;
+    this.database = database;
+    this.connection = null;
+  }
+
+  // open sql connection
+  public boolean openConnection() {
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      this.connection = DriverManager.getConnection(
+          "jdbc:mysql://" + host + ":" + port + "/" + database + "?autoReconnect=true", user,
+          password);
+      return true;
+    } catch (SQLException | ClassNotFoundException e) {
+      DepositVault.getInstance().getLogger().severe("Could not connect to MySQL server! because: "
+          + e.getMessage());
+    }
+    return false;
+  }
+
+}
