76aea51675eefe0df7a1a165d5f93a1be3576824
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-03-26 00:32:26 +0900

    Create meta table

--- src/main/java/com/skuralll/depositvault/db/Database.java
+++ src/main/java/com/skuralll/depositvault/db/Database.java
@@ -4,8 +4,11 @@ import com.skuralll.depositvault.DepositVault;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Database {
+
+  public final static String DB_VERSION = "1";
 
   // Database connection information
   private String host;
@@ -33,12 +36,28 @@ public class Database {
       this.connection = DriverManager.getConnection(
           "jdbc:mysql://" + host + ":" + port + "/" + database + "?autoReconnect=true", user,
           password);
+      createTables();
       return true;
     } catch (SQLException | ClassNotFoundException e) {
       DepositVault.getInstance().getLogger().severe("Could not connect to MySQL server! because: "
           + e.getMessage());
     }
     return false;
+  }
+
+  // create tables if not exist
+  private void createTables() throws SQLException {
+    Statement statement = connection.createStatement();
+    // db meta table
+    statement.executeUpdate(
+        "CREATE TABLE IF NOT EXISTS `meta` (" +
+            "`key`   TEXT NOT NULL," +
+            "`value` TEXT NOT NULL" +
+            ")"
+    );
+    statement.executeUpdate(
+        "INSERT INTO `meta` SELECT 'dbversion','" + DB_VERSION + "' "
+            + " WHERE NOT EXISTS (SELECT `key` FROM `meta` WHERE `key` = 'dbversion')");
   }
 
 }
