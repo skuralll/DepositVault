a4bd967e359cf6392d37da72e8313a45fe9aad20
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-03-28 16:22:06 +0900

    Add Info Command

---
+++ src/main/java/com/skuralll/depositvault/command/InfoCommand.java
@@ -0,0 +1,25 @@
+package com.skuralll.depositvault.command;
+
+import com.skuralll.depositvault.DepositVault;
+import com.skuralll.depositvault.db.Database;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.PluginDescriptionFile;
+
+public class InfoCommand extends SubCommand {
+
+  private final PluginDescriptionFile description;
+
+  public InfoCommand() {
+    description = DepositVault.getInstance().getDescription();
+  }
+
+  @Override
+  public boolean onCommand(CommandSender sender, String[] args) {
+    sender.sendMessage(description.getName() + " : v" + description.getVersion());
+    sender.sendMessage("Authors : " + String.join(",", description.getAuthors()));
+    sender.sendMessage("Website : " + description.getWebsite());
+    sender.sendMessage("DB-Version : v" + Database.DB_VERSION);
+    return true;
+  }
+
+}
