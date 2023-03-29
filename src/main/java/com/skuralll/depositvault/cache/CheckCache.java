a55ea77652aa03cac97183fa8a2fdbb70b9fadd7
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-03-29 15:33:28 +0900

    Implements get and check method on CheckCache

--- src/main/java/com/skuralll/depositvault/cache/CheckCache.java
+++ src/main/java/com/skuralll/depositvault/cache/CheckCache.java
@@ -4,6 +4,7 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.CheckForNull;
 
 public class CheckCache {
 
@@ -17,7 +18,22 @@ public class CheckCache {
   }
 
   public void put(UUID uuid, Long time) {
-    cache.put(uuid, time);
+    cache.put(uuid, System.currentTimeMillis() + time);
+  }
+
+  @CheckForNull
+  public Long get(UUID uuid) {
+    return cache.getIfPresent(uuid);
+  }
+
+  public boolean check(UUID uuid) {
+    Long time = get(uuid);
+    if (time == null)
+      return false;
+    if (time <= System.currentTimeMillis())
+      return false;
+    cache.invalidate(uuid);
+    return true;
   }
 
 }
