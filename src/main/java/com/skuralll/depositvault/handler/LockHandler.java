f43739ee924a8422cdf48cb97b621bcba32b3e24
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-04-09 22:46:44 +0900

    Show username in check command

--- src/main/java/com/skuralll/depositvault/handler/LockHandler.java
+++ src/main/java/com/skuralll/depositvault/handler/LockHandler.java
@@ -43,14 +43,18 @@ public class LockHandler {
   }
 
   public String getLockDataMessage(Location location) {
-//    DepositData deposit_data = getPurchaseCost();
     LockData lock_data = getLockData(location);
+
+    int user_id = lock_data.getUserId();
+    String user_name = getUserName(user_id);
+
     String message = "";
     message += "[Status]" + "\n";
     message += "Locked: " + (lock_data != null ? "Yes" : "No") + "\n";
     if (lock_data != null) {
       message += "Expiration: " + lock_data.getExpireDate().toString() + "\n";
-      message += "User ID: " + lock_data.getUserId() + "\n";
+      message += "User: " + (user_name != null ? user_name : "UNKNOWN") + " (ID:"
+          + lock_data.getUserId() + ")\n";
       message += "Lock ID: " + lock_data.getLockId() + "\n";
     }
     return message;
