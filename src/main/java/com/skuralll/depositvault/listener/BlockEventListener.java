c17d911f04f750d531f591cd5ecd15b34a0aa951
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-04-14 14:02:08 +0900

    Fixed that already locked chests cannot be made into large chests

--- src/main/java/com/skuralll/depositvault/listener/BlockEventListener.java
+++ src/main/java/com/skuralll/depositvault/listener/BlockEventListener.java
@@ -5,10 +5,15 @@ import com.skuralll.depositvault.handler.LockHandler;
 import com.skuralll.depositvault.model.LockData;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.type.Chest;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockEventListener implements Listener {
 
@@ -47,6 +52,45 @@ public class BlockEventListener implements Listener {
           owner_name = "Unknown";
         player.sendMessage(ChatColor.RED + "This block is locked by " + owner_name + ".");
       }
+    }
+  }
+
+  private final int[][] arounds = new int[][]{
+          {-1,-1},
+          {-1,0},
+          {-1,1},
+          {0,-1},
+          {0,1},
+          {1,-1},
+          {1,0},
+          {1,1}
+  };
+
+  @EventHandler
+  public void onBlockPlace(BlockPlaceEvent event) {
+    Block block = event.getBlock();
+    // check is block chest
+    if (!block.getType().equals(Material.CHEST))
+      return;
+    BlockFace direction = ((Chest) block.getBlockData()).getFacing();
+
+    Location location = block.getLocation();
+    // protect locked inventory-holder
+    for(int[] pos : arounds){
+      // check is around block chest
+      Location around_location = location.clone().add(pos[0], 0, pos[1]);
+      Block around_block = around_location.getBlock();
+      if (!around_block.getType().equals(Material.CHEST))
+        continue;
+      // check is around block same direction
+      if(!((Chest) around_block.getBlockData()).getFacing().equals(direction))
+        continue;
+      // check is around block locked
+      LockData lock_data = handler.getLockData(around_location);
+      if (lock_data == null)
+        continue;
+      // event cancel
+      event.setCancelled(true);
     }
   }
 
